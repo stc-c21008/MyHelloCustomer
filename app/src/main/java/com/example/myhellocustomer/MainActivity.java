ffab1905c7fc8c2b9f939e6a7a2f4bf6cf3c61a6
c21008 <c21008@suntech.jp>
2022-06-28 11:57:08 +0900

    Costomer

--- app/src/main/java/com/example/myhellocustomer/MainActivity.java
+++ app/src/main/java/com/example/myhellocustomer/MainActivity.java
@@ -32,24 +32,24 @@ public class MainActivity extends AppCompatActivity {
         //イベントハイドラ　<= イベント発生時
         @Override
         public void onClick(View view) {
-            EditText inlive = findViewById(R.id.etLive);
-            EditText inname = findViewById(R.id.etName);
-            TextView output = findViewById(R.id.tvOutput);
+            EditText inlive = findViewById(R.id.etLive);    //入力した住所を挿入
+            EditText inname = findViewById(R.id.etName);    //入力した名前を挿入
+            TextView output = findViewById(R.id.tvOutput);  //出力関数
 
             int id = view.getId();
-            switch (id) {
-                case R.id.btClick:
-                    String inliveStr = inlive.getText().toString();
-                    String innameStr = inname.getText().toString();
-                    output.setText(inliveStr + "にお住いの" + innameStr + "さん、こんにちは！");
+            switch (id) {   //ボタン入力
+                case R.id.btClick:  //表示ボタン
+                    String inliveStr = inlive.getText().toString(); //住所を文字列型へ
+                    String innameStr = inname.getText().toString(); //名前を文字列型へ
+                    output.setText(inliveStr + "にお住いの" + innameStr + "さん、こんにちは！");  //出力
                     break;
 
-                case R.id.btLive:
+                case R.id.btLive:   //住所クリア
                     inlive.setText("");
                     output.setText("");
                     break;
 
-                case R.id.btName:
+                case R.id.btName:   //名前クリア
                     inname.setText("");
                     output.setText("");
                     break;
